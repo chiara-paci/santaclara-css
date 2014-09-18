a3b9c5fe87d7cfe422c2badc998ad6d1186c76b4
Chiara Paci <chiara.paci@gmail.com>
2014-09-18 16:19:37 +0200

    Added colors 6

---
+++ santaclara_css/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+#from django.views.generic import simple
+
+from santaclara_css.views import CssTemplateView
+
+urlpatterns =patterns('',
+                      ( r'base.css$',        CssTemplateView.as_view(template_name='santaclara_css/base.css') ),
+                      )
+
+
+
