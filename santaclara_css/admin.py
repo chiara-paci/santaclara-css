a07f802651b38905e5517190eb5bb3803df07c64
Chiara Paci <chiara.paci@gmail.com>
2014-09-19 10:14:43 +0200

    Added colors 19

--- santaclara_css/admin.py
+++ santaclara_css/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from santaclara_css.models import CssColor
 
 class CssColorAdmin(admin.ModelAdmin):
-    list_display=[ "name","__unicode__","hexadecimal","alpha","red","green","blue"]
+    list_display=[ "name","hexadecimal","red","green","blue"]
 
 admin.site.register(CssColor,CssColorAdmin)
