8bc3866be52cc8673ab3ab438b33529ef192ab05
Chiara Paci <chiara.paci@gmail.com>
2014-09-19 10:13:29 +0200

    Added colors 18

--- santaclara_css/models.py
+++ santaclara_css/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 from django.core import validators
 
-
-# Create your models here.
-
 class CssColor(models.Model):
     name = models.SlugField(unique=True)
     hexadecimal=models.CharField(max_length=6,blank=True,
@@ -15,10 +12,6 @@ class CssColor(models.Model):
                                                     validators.MaxValueValidator(255)],blank=True)
     blue = models.PositiveIntegerField(validators=[validators.MinValueValidator(0),
                                                    validators.MaxValueValidator(255)],blank=True)
-    alpha = models.FloatField(validators=[validators.MinValueValidator(0.0),
-                                          validators.MaxValueValidator(1.0)],
-                              default=1.0)
-
     
     def save(self,*args,**kwargs):
         if not self.red: self.red=0
@@ -37,10 +30,25 @@ class CssColor(models.Model):
         super(CssColor, self).save(*args, **kwargs)
 
     def __unicode__(self):
+        U= u"%d,%d,%d" % (self.red,self.green,self.blue)
+        return U
+
+class CssColorVariable(models.Model):
+    name = models.SlugField(unique=True)
+    color = models.ForeignKey(CssColor)
+    alpha = models.FloatField(validators=[validators.MinValueValidator(0.0),
+                                          validators.MaxValueValidator(1.0)],
+                              default=1.0)
+
+    def save(self,*args,**kwargs):
+        self.name=self.name.upper()
+        super(CssColorVariable, self).save(*args, **kwargs)
+
+    def __unicode__(self):
         U=u"rgb"
         if self.alpha!=1.0:
             U+=u"a"
-        U+= u"(%d,%d,%d" % (self.red,self.green,self.blue)
+        U+= u"(%s" % unicode(self.color)
         if self.alpha!=1.0:
             U+=u",%2.2f" % self.alpha
         U+=u")"
