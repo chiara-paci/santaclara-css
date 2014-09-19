88080c99ff5d557bbdad374980ecd0c14052407d
Chiara Paci <chiara.paci@gmail.com>
2014-09-19 13:29:19 +0200

    Added colors 36

--- santaclara_css/admin.py
+++ santaclara_css/admin.py
@@ -3,18 +3,28 @@ from django.contrib import admin
 from santaclara_css.models import CssColor,CssColorVariable
 
 class CssColorAdmin(admin.ModelAdmin):
-    list_display=[ "hexadecimal","name","red","green","blue"]
+    list_display=[ "hexadecimal","name","color_box","red","green","blue"]
     list_editable=[ "name" ]
+    
+    def color_box(self,obj):
+        return format_html('<span style="background: rgb({0}); width: 3em; height: 1em;">&nbsp;</span>',
+                           obj.rgb())
+
+    colored_name.allow_tags = True
 
 admin.site.register(CssColor,CssColorAdmin)
 
 class CssColorVariableAdmin(admin.ModelAdmin):
-    list_display=[ "name","alpha","color","rgb"]
+    list_display=[ "name","color_box","alpha","color","rgb"]
     list_editable=["color"]
 
     def rgb(self, obj):
         return obj.color.rgb()
     rgb.admin_order_field  = 'color'
     rgb.short_description = 'RGB'
+
+    def color_box(self,obj):
+        return format_html('<span style="background: {0}; width: 3em; height: 1em;">&nbsp;</span>',
+                           obj.color.rgb())
 
 admin.site.register(CssColorVariable,CssColorVariableAdmin)
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='santaclara_css',
-    version='0.2.2.35',
+    version='0.2.2.36',
     packages=['santaclara_css'],
     package_data={'santaclara_css': [
             'static/css/*.css',
