db96bebc8b9bd225ee45fd463f91428a98aa7d2a
Chiara Paci <chiara.paci@gmail.com>
2014-12-28 15:04:06 +0100

    Added color equivalence 11

--- santaclara_css/context_processors.py
+++ santaclara_css/context_processors.py
@@ -11,3 +11,12 @@ def shadows(request=None):
     for shadow in CssShadowVariable.objects.all():
         T["SHADOW_"+unicode(shadow.name)]=unicode(shadow)
     return T
+
+def equivalence_colors(request=None):
+    T={}
+    for equivalence in CssEquivalenceColorVariable.objects.all():
+        name=unicode(equivalence.name)
+        for eq_color in equivalence.cssequivalencecolor_set.all():
+            style=unicode(eq_color.style).upper()
+            T["COLOR_"+style+"_"+name]=equivalence.color_desc(eq_color.color)
+    return T
