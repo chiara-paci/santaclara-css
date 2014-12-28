c9dce14c735a5baa324c16cc2ed445defbddac2a
Chiara Paci <chiara.paci@gmail.com>
2014-12-28 12:05:17 +0100

    Added template_vars_delegate 7

---
+++ docs/commands.md
@@ -0,0 +1,13 @@
+#Commands
+
+##add_css_color name color
+
+Color:
+- transparent
+- #hexadecimal
+- rgb(red,green,blue)
+- rgba(red,green,blue,alpha)
+
+##update_palette name color0 color+2(ligth) color+1 color-1 color-2(dark)
+
+Colors are specified in hexadecimal format (without #)
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='santaclara_css',
-    version='0.3.1.6',
+    version='0.3.1.7',
     packages=['santaclara_css'],
     package_data={'santaclara_css': [
             'static/css/*.css',
