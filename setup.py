f5c4be627a4a451d1be3cfc990ced366e6a5429d
Chiara Paci <chiara.paci@gmail.com>
2014-12-28 15:45:13 +0100

    Added color equivalence 24

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='santaclara_css',
-    version='0.4.1.23',
+    version='0.4.1.24',
     packages=['santaclara_css'],
     package_data={'santaclara_css': [
             'static/css/*.css',
