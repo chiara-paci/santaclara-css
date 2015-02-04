a961f485db1a95994ad6e93e8d30b562f34f9811
Chiara Paci <chiara.paci@gmail.com>
2015-02-04 13:35:05 +0100

    Add to common.css 1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='santaclara_css',
-    version='0.5.1.2',
+    version='0.5.2.1',
     packages=['santaclara_css'],
     package_data={'santaclara_css': [
             'static/santaclara_css/*.css',
