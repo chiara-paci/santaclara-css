fc68cd4a79c289fb5bca64f79af0e9abe5965138
Chiara Paci <chiara.paci@gmail.com>
2015-03-04 15:51:36 +0100

    Added EquivalenceStanza 55

--- santaclara_css/admin.py
+++ santaclara_css/admin.py
@@ -136,7 +136,7 @@ admin.site.register(CssEquivalenceLinearGradient,CssEquivalenceLinearGradientAdm
 class CssEquivalenceSelectorStanzaThroughInline(admin.TabularInline):
     model = CssEquivalenceStanza.selectors.through
     extra = 0
-    fields = admin.TabularInline.fields+('css_text')
+    fields = ('stanza','css_text')
     readonly_fields = ( 'css_text', )
 
     def css_text(self,instance): 
